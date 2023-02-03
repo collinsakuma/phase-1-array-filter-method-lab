c04e12b22023e6ff40f0c20b8b4a08f8eb54b080
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-03 07:38:44 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+
+// function findMatching(drivers, driver) {
+//     function isDriver(name) {
+//         return(name.toLowerCase() == driver.toLowerCase());
+//     }
+//     return drivers.filter(isDriver);
+// }
+
+
+function findMatching(drivers, driver) {
+    return drivers.filter(name => name.toLowerCase() === driver.toLowerCase())
+}
+
+// function fuzzyMatch(drivers, driver) {
+//     function isDriver(name) {
+//         return (`${name[0]}${name[1]}` === driver);
+//     }
+//     return drivers.filter(isDriver);
+// }
+function fuzzyMatch(drivers,driver) {
+    return drivers.filter(name => `${name[0]}${name[1]}` === driver);
+}
+
+function matchName(drivers, input) {
+    return drivers.filter(driver => driver.name === input)
+}
+
